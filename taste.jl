e64b14fdb863d0d3544838f43b3456adec9f41be
milktrader <milktrader@gmail.com>
2012-12-09 22:37:40 -0500

    renamed saute to taste

--- taste.jl
+++ taste.jl
@@ -1,6 +1,4 @@
 macro taste(food)
-  fresh  = 0 
-  rotten = 0
   :($food ? 
           print("\33[32myum\033[0m ")  :
           print("\33[31mYUK!\033[0m "))
---
+++ wrapme.jl
@@ -0,0 +1,12 @@
+
+function wrapme()
+
+fresh = 0
+rotten = 0
+
+@tasteadd 1==1
+@tasteadd 2==2
+
+fresh
+end
+
